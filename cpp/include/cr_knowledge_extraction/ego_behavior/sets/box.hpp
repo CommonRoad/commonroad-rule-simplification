d6b6aed00acea75d16fdc24846f252d95428fe96
Florian Lercher <florian.lercher@tum.de>
2025-02-07 17:24:43 +0100

    Add documentation for boxes

--- cpp/include/cr_knowledge_extraction/ego_behavior/sets/box.hpp
+++ cpp/include/cr_knowledge_extraction/ego_behavior/sets/box.hpp
@@ -9,21 +9,45 @@ template <int N> struct Box {
     const Eigen::Vector<double, N> center;
     const Eigen::Vector<double, N> radius;
 
+    /**
+     * Create a box from its center and radius.
+     *
+     * @param center The center point of the box.
+     * @param radius The radius of the box in each dimension.
+     */
     Box(const Eigen::Vector<double, N> &center, const Eigen::Vector<double, N> &radius)
         : center(center), radius(radius) {
         assert((radius.array() >= 0).all());
     }
 
-    static Box<N> from_bounds(const Eigen::Vector<double, N> &min, const Eigen::Vector<double, N> &max) {
+    /**
+     * Create a box from its bounds, i.e., the lower left and upper right corner (generalized to N dimensions).
+     *
+     * @param min The lower left corner of the box.
+     * @param max The upper right corner of the box.
+     * @return The box defined by the given bounds.
+     */
+    static Box from_bounds(const Eigen::Vector<double, N> &min, const Eigen::Vector<double, N> &max) {
         auto radius = (max - min) / 2;
         return Box{min + radius, radius};
     }
 
+    /**
+     * Get the bounds of the box, i.e., the lower left and upper right corner (generalized to N dimensions).
+     *
+     * @return The bounds of the box.
+     */
     std::pair<Eigen::Vector<double, N>, Eigen::Vector<double, N>> bounds() const {
         return {center - radius, center + radius};
     }
 
-    Box<N> intersect(const Box<N> &other) const {
+    /**
+     * Check whether two boxes intersect.
+     *
+     * @param other The other box.
+     * @return True iff the boxes intersect.
+     */
+    Box intersect(const Box &other) const {
         auto [own_min, own_max] = bounds();
         auto [other_min, other_max] = other.bounds();
         auto min = own_min.cwiseMax(other_min);
@@ -35,22 +59,51 @@ template <int N> struct Box {
         }
     }
 
-    Box<N> sum(const Box<N> &other) const { return Box{center + other.center, radius + other.radius}; }
+    /**
+     * Compute the Minkowski sum of two boxes.
+     *
+     * @param other The other summand.
+     * @return The box representing the Minkowski sum.
+     */
+    Box sum(const Box &other) const { return Box{center + other.center, radius + other.radius}; }
 
-    Box<N> shrink(const double &delta) const { return Box{center, (radius.array() - (delta / 2)).cwiseMax(0)}; }
+    /**
+     * Shrink a box by a given amount in each dimension.
+     *
+     * @param delta The amount to shrink the box by in each dimension.
+     * @return The shrunken box.
+     */
+    Box shrink(const double &delta) const { return Box{center, (radius.array() - (delta / 2)).cwiseMax(0)}; }
 
+    /**
+     * Compute the linear map of the box by a given matrix.
+     *
+     * The matrix must not contain negative entries.
+     * Note that this operation results in an overapproximation.
+     *
+     * @tparam M Output dimension of the linear map.
+     * @param matrix The non-negative matrix to use for the linear map.
+     * @return The box overapproximating the result of the linear map.
+     */
     template <int M> Box<M> linear_map_positive(const Eigen::Matrix<double, M, N> &matrix) const {
         assert((matrix.array() >= 0).all());
         return Box<M>{matrix * center, matrix * radius};
     }
 
-    bool operator==(const Box<N> &other) const { return center == other.center && radius == other.radius; }
+    bool operator==(const Box &other) const = default;
 };
 
+/**
+ * Alias for a two-dimensional box.
+ */
 using Box2D = Box<2>;
+
+/**
+ * Alias for a four-dimensional box.
+ */
 using Box4D = Box<4>;
 
-template <int N> inline std::ostream &operator<<(std::ostream &os, const Box<N> &box) {
+template <int N> std::ostream &operator<<(std::ostream &os, const Box<N> &box) {
     os << "Box{ center:\n" << box.center << "\nradius:\n" << box.radius << "}";
     return os;
 }
